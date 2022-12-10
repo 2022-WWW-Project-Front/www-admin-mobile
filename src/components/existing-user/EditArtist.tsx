b0302b898293a36240fad30b6c4ae290aa886f49
whilini <whi0306@naver.com>
2022-12-11 02:41:56 +0900

    feat: edit artist info page

--- src/components/existing-user/EditArtist.tsx
+++ src/components/existing-user/EditArtist.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EditArtistLayout from '../../layout/existing-user/EditArtistLayout';
+import { useNavigate } from 'react-router-dom';
+
+const ALLOW_FILE_EXTENSION = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
 
 const EditArtist = () => {
-  return <div>EditArtist</div>;
+  const navigate = useNavigate();
+  const [artistInfo, setArtistInfo] = useState({
+    profile:
+      'https://avatars.githubusercontent.com/u/98644969?s=400&u=473c711eeed5bf1453e9e7522718ed012ece3235&v=4',
+    title: '작가소개 공백포함 16자제한',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 공백포한 700자로 제한합니다......`
+  });
+
+  const encodeFileToBase64 = (event: { target: HTMLInputElement }) => {
+    const target = event.target;
+    const files = target.files as FileList;
+    const fileLoaded = URL.createObjectURL(files[0]);
+
+    console.log('files: ', files[0]);
+    if (!ALLOW_FILE_EXTENSION.exec(target.value)) {
+      alert('파일 형식을 확인해주세요.');
+      return;
+    }
+    setArtistInfo({ ...artistInfo, profile: fileLoaded });
+  };
+
+  const backToMenu = () => {
+    navigate('/artist');
+  };
+
+  const changeInfo = () => {
+    // forward to server
+    navigate('/artist');
+  };
+
+  return (
+    <EditArtistLayout
+      artistInfo={artistInfo}
+      setArtistInfo={setArtistInfo}
+      encodeFileToBase64={encodeFileToBase64}
+      backToMenu={backToMenu}
+      changeInfo={changeInfo}
+    />
+  );
 };
 
 export default EditArtist;
--- src/layout/existing-user/EditArtistLayout.tsx
+++ src/layout/existing-user/EditArtistLayout.tsx
@@ -1,7 +1,158 @@
 import React from 'react';
+import styled from 'styled-components';
+import CloseBtn from '../common/icon/CloseBtn';
 
-const EditArtistLayout = () => {
-  return <div>EditArtist</div>;
+interface EditArtistDetailProps {
+  artistInfo: {
+    profile: string;
+    title: string;
+    description: string;
+  };
+  setArtistInfo: (artistInfo: { profile: string; title: string; description: string }) => void;
+  encodeFileToBase64: (event: { target: HTMLInputElement }) => void;
+  backToMenu: () => void;
+  changeInfo: () => void;
+}
+
+const EditArtistLayout = ({
+  artistInfo,
+  setArtistInfo,
+  encodeFileToBase64,
+  backToMenu,
+  changeInfo
+}: EditArtistDetailProps) => {
+  const { profile, title, description } = artistInfo;
+  return (
+    <ArtistDetailContainer>
+      <ProfileImgContainer>
+        <ImgFile htmlFor="profile" imageSrc={profile}>
+          <div>
+            <img src="/assets/profileBox.svg" alt="profile camera icon" />
+          </div>
+        </ImgFile>
+        <input type="file" id="profile" onChange={encodeFileToBase64} />
+        <BtnBox onClick={backToMenu}>
+          <CloseBtn />
+        </BtnBox>
+      </ProfileImgContainer>
+      <ContentContainer>
+        <input
+          type={'text'}
+          value={title}
+          onChange={(e) => setArtistInfo({ ...artistInfo, title: e.target.value })}
+        />
+        <textarea
+          value={description}
+          onChange={(e) => setArtistInfo({ ...artistInfo, description: e.target.value })}
+        />
+      </ContentContainer>
+      <NextBtn onClick={changeInfo}>수정완료</NextBtn>
+    </ArtistDetailContainer>
+  );
 };
 
 export default EditArtistLayout;
+
+const ArtistDetailContainer = styled.div`
+  overflow: auto;
+  inset: 0;
+  width: 100%;
+  min-height: 100vh;
+  background-color: var(--sub);
+`;
+
+const ProfileImgContainer = styled.div`
+  padding-top: 1.3rem;
+  height: 20.264rem;
+  border-bottom: 1px var(--black-400) solid;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  input {
+    display: none;
+  }
+`;
+
+const ImgFile = styled.label<{ imageSrc: string }>`
+  width: 14.375rem;
+  height: 14.375rem;
+  border: 1px var(--black-100) solid;
+  box-sizing: border-box;
+  background-image: ${({ imageSrc }) => `url(${imageSrc})`};
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  margin-top: 1.125rem;
+  div {
+    position: absolute;
+    z-index: 3;
+    right: 0;
+    bottom: 0;
+    margin: 0;
+    width: 2.875rem;
+    height: 2.875rem;
+    border-radius: 0.19rem;
+    border: 1px var(--black-200) solid;
+    background-color: var(--sub);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 1.875rem;
+    }
+  }
+`;
+
+const ContentContainer = styled.div`
+  margin: 1.5rem 1.25rem 4rem;
+  display: block;
+  overflow: auto;
+  height: fit-content;
+  position: relative;
+  input {
+    padding: 0.5rem 0.75rem;
+    width: calc(100% - 1.5rem);
+    font-size: 1.5rem;
+    font-weight: 600;
+    border: none;
+    border-bottom: 1px var(--black-200) solid;
+  }
+  textarea {
+    padding: 0.5rem 0.75rem;
+    margin-top: 0.75rem;
+    line-height: 1.5rem;
+    width: 100%;
+    height: 28.25rem;
+    border: 1px var(--black-200) solid;
+    box-sizing: border-box;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`;
+
+const BtnBox = styled.div`
+  position: absolute;
+  z-index: 200;
+  top: 0;
+  right: 0;
+`;
+
+const NextBtn = styled.button`
+  width: calc(100% - 2.5rem);
+  margin: 5.25rem 1.25rem 1.25rem;
+  height: 4.375rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--black-400);
+  color: var(--white);
+  font-weight: 600;
+  font-size: 1.25rem;
+  border: none;
+`;
